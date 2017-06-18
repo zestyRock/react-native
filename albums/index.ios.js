24dd61ecfe74d71df42056d9d91ffd2a80c13b7a
ZestyRock <yanguo1208@gmail.com>
2017-06-18 10:37:04 -0700

    [albums][exercise] init repo first commit

---
+++ albums/index.ios.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+  <Header headerText={'Albums'} />
+);
+
+AppRegistry.registerComponent('albums', () => App);
