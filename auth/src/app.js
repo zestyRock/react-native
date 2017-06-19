e0d303d3e622b4c2a43554f1c46428b3e15aa582
ZestyRock <yanguo1208@gmail.com>
2017-06-19 09:29:28 -0700

    auth app first commit init repo

---
+++ auth/src/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App </Text>
+      </View>
+    )
+  }
+}
+export default App;
